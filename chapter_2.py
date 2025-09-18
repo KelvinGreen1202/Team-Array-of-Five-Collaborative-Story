6d719e0ad9611fe4063d28ce08ff5a9d11a01e71
JavidHanmore <hanmorejavid@gmail.com>
2025-09-18 14:21:29 -0500

    Create chapter_2.py
    Added Chapter 2

---
+++ chapter_2.py
@@ -0,0 +1,15 @@
+# chapter_2.py
+
+def demon_scene():
+    print("The demon was completely stuck in space.")
+    print("He moved his big eyes around in fear.")
+    print("He uttered, 'Once I get ouf of this, I'll-'")
+    print("Ivy laughs and snaps her fingers.")
+    print("The demon began to vibrate violently and then shattered into pieces.")
+
+def  aftermath_scene():
+    print("Suddenly there was a large black cloud forming.")
+    print("Ivy sighed, 'Here comes the rest of them.'")
+
+demon_scene()
+aftermath_scene()
