e7d55cb9913e2fa70e3f22db3f5fe239096d7283
Tac0Bo0ts <theonethatcame0@gmail.com>
2025-09-18 22:15:14 -0500

    Update Chapter_4.py

--- Chapter_4.py
+++ Chapter_4.py
@@ -4,7 +4,7 @@ def demon_description():
     print("The demon reassembled from his shattered pieces however he was not like how he was once before.")
     print("The pieces slowly grew three times larger and took the resemblance of armor now.")
     print("These pieces slowly came together, rebuilding the demons body from his feet to his head")
-    print("At the end of the rebuild the demon stood tall at about 18 feet in height.")
+    print("At the end of the rebuild the demon stood at about 18 feet tall.")
     print("He also had this thick coating of dark red titanium armor with a massive halberd in his hands")
 def final_battle_preparation():
     print("The demon spoke in a clear deep voice to Ivy and her allies")
