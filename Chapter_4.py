9af45c8ecca7e5f5af7344ba9ee431c6bd549ecd
Tac0Bo0ts <theonethatcame0@gmail.com>
2025-09-18 22:10:48 -0500

    Update Chapter_4.py

--- Chapter_4.py
+++ Chapter_4.py
@@ -7,5 +7,13 @@ def demon_description():
     print("At the end of the rebuild the demon stood tall at about 18 feet in height.")
     print("He also had this thick coating of dark red titanium armor with a massive halberd in his hands")
 def final_battle_preparation():
-    print("The demon spoke in a clear deep voice to
+    print("The demon spoke in a clear deep voice to Ivy and her allies")
+    print("'I do not think you understand what you are dealing with.'")
+    print("'I am no ordinary demon. I do not need an army like you weak humans'")
+    print("Ivy responded saying 'You underestimate us and that will be your downfall'")
+    print("Both Ivy and the demon stare at each other for a couple of seconds without saying anything")
+    print("Neither side wants to make any move when all of a sudden they both charge in")
+
+demon_description()
+final_battle_preparation()
     
